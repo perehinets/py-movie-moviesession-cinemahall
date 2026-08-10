4965938f269a4daf5e91e92d0184ee6d1aed2732
Vasyl Perehinets <perehinets95@gmail.com>
2026-08-10 23:57:57 +0100

    fix:2

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 from db.models import MovieSession, Movie, CinemaHall
 
@@ -14,7 +14,7 @@ def create_movie_session(movie_show_time: datetime,
     )
 
 
-def get_movies_sessions(session_date: datetime.datetime = None) -> MovieSession:
+def get_movies_sessions(session_date: datetime = None) -> MovieSession:
     queryset = MovieSession.objects.all()
     if session_date:
         return queryset.filter(show_time__date=session_date)
@@ -26,7 +26,7 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 
 def update_movie_session(session_id: int,
-                         show_time: datetime.datetime = None,
+                         show_time: datetime = None,
                          movie_id: int = None,
                          cinema_hall_id: int = None
                          ) -> None:
