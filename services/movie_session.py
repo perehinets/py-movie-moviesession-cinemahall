fe0f8c49812b69259641f3b8cbd83d7c1a3a1f2f
Vasyl Perehinets <perehinets95@gmail.com>
2026-08-11 12:00:41 +0100

    changed on advice

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,20 +1,20 @@
 from datetime import datetime
 
-from db.models import MovieSession, Movie, CinemaHall
+from db.models import MovieSession
 
 
 def create_movie_session(movie_show_time: datetime,
                          movie_id: int,
                          cinema_hall_id: int
-                         ) -> None:
-    MovieSession.objects.create(
+                         ) -> MovieSession:
+    return MovieSession.objects.create(
         show_time=movie_show_time,
-        cinema_hall=CinemaHall.objects.get(id=cinema_hall_id),
-        movie=Movie.objects.get(id=movie_id)
+        cinema_hall_id=cinema_hall_id,
+        movie_id=movie_id
     )
 
 
-def get_movies_sessions(session_date: datetime = None) -> MovieSession:
+def get_movies_sessions(session_date: str | None = None) -> MovieSession:
     queryset = MovieSession.objects.all()
     if session_date:
         return queryset.filter(show_time__date=session_date)
@@ -26,17 +26,17 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 
 def update_movie_session(session_id: int,
-                         show_time: datetime = None,
-                         movie_id: int = None,
-                         cinema_hall_id: int = None
+                         show_time: datetime | None = None,
+                         movie_id: int | None = None,
+                         cinema_hall_id: int | None = None
                          ) -> None:
     session = MovieSession.objects.get(id=session_id)
     if show_time:
         session.show_time = show_time
     if movie_id:
-        session.movie = Movie.objects.get(id=movie_id)
+        session.movie_id = movie_id
     if cinema_hall_id:
-        session.cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+        session.cinema_hall_id = cinema_hall_id
     session.save()
 
 
