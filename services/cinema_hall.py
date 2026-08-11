d44c3e848eb09254fcd7f23e1b974d703e7ea3d0
Vasyl Perehinets <perehinets95@gmail.com>
2026-08-11 15:54:55 +0100

    changed on the advice of Luk

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -3,15 +3,14 @@ from django.db.models import QuerySet
 
 
 def get_cinema_halls() -> QuerySet:
-    cinema_halls = CinemaHall.objects.all()
-    return cinema_halls
+    return CinemaHall.objects.all()
 
 
 def create_cinema_hall(hall_name: str,
                        hall_rows: int,
                        hall_seats_in_row: int
                        ) -> None:
-    CinemaHall(name=hall_name,
-               rows=hall_rows,
-               seats_in_row=hall_seats_in_row
-               ).save()
+    return CinemaHall(name=hall_name,
+                      rows=hall_rows,
+                      seats_in_row=hall_seats_in_row
+                      ).save()
--- services/movie.py
+++ services/movie.py
@@ -1,22 +1,26 @@
+from django.db.models import QuerySet
+
 from db.models import Movie
 from typing import List
 
 
-def get_movies(genres_ids: List[int] = None,
-               actors_ids: List[int] = None,
-               ) -> List[Movie] :
+def get_movies(genres_ids: List[int] | None = None,
+               actors_ids: List[int] | None = None,
+               ) -> QuerySet :
     queryset = Movie.objects.all()
     if genres_ids is None and actors_ids is None:
         return queryset
 
     if genres_ids and actors_ids is None:
-        return queryset.filter(genres__in=genres_ids)
+        return queryset.filter(genres__id__in=genres_ids).distinct()
 
     if actors_ids and genres_ids is None:
-        return queryset.filter(actors__in=actors_ids)
+        return queryset.filter(actors__id__in=actors_ids).distinct()
 
     if genres_ids and actors_ids:
-        return queryset.filter(genres__in=genres_ids, actors__in=actors_ids)
+        return queryset.filter(genres__id__in=genres_ids,
+                               actors__id__in=actors_ids
+                               ).distinct()
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
@@ -25,8 +29,8 @@ def get_movie_by_id(movie_id: int) -> Movie:
 
 def create_movie(movie_title: str,
                  movie_description: str,
-                 actors_ids: list = None,
-                 genres_ids: list = None
+                 actors_ids: list | None = None,
+                 genres_ids: list | None = None
                  ) -> None:
     movie = Movie.objects.create(title=movie_title,
                                  description=movie_description
